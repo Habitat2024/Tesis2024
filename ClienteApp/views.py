a4520affc8f12f46bc2e1f92cff6cfc7e2fe1a66
Romario Villalobos <romarioavr@gmail.com>
2024-03-10 11:16:52 -0600

    oculatar fiador 1.1

--- ClienteApp/views.py
+++ ClienteApp/views.py
@@ -815,17 +815,17 @@ def consulta_evaliacion_natural(request):
         except Exception:
            listaId.append("-0")
         try:
-            datos_fiador = DatosPersFia.objects.get(IdSolicitud=solicitud.Id )
-            if(datos_fiador.Tipo == "codeudor"):
-                try:
+            datos_fiador = DatosPersFia.objects.get(IdSolicitud=solicitud.Id,tipo = "codeudor")
+            
+            try:
                     conosca_cliente_fiador=ClienteDatoGen.objects.get(IdSolicitud=solicitud.Id, CalidadActu="Fiador")
                     listaId.append(conosca_cliente_fiador.Id)
-                except Exception:
+            except Exception:
                     listaId.append("-0")
-            else:
-                listaId.append("-1") 
+            
         except Exception:
            listaId.append("-1") 
+
         print("nat  "+str(listaId)+" p= "+id)
         serialized_data = json.dumps(listaId, default=str)
         return HttpResponse(serialized_data, content_type="application/json")
